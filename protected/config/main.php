5afb774ce6808db229ee75b71a02b04db20dee61
Tam Nguyen <minhtam220@gmail.com>
2012-02-22 11:33:32 +0700

    Tam

--- protected/config/main.php
+++ protected/config/main.php
@@ -67,7 +67,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=lintinzone',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 			'tablePrefix'=>'lz_',
 		
