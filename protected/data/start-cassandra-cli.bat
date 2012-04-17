f64839a9869959be849958637fa3c3929963fea6
Tung Tran Thanh <tung121089@gmail.com>
2012-04-17 08:16:49 +0700

    Two bat file to quickly start cassandra server. You should edit these files to suit your filepath.

---
+++ protected/data/start-cassandra-cli.bat
@@ -0,0 +1,2 @@
+cd /D D:\wamp\bin\cassandra\apache-cassandra-1.0.8\conf
+..\bin\cassandra-cli -h 127.0.0.1 -p 9160
---
+++ protected/data/start-cassandra-server.bat
@@ -0,0 +1,2 @@
+cd /D D:\wamp\bin\cassandra\apache-cassandra-1.0.8\conf
+..\bin\cassandra -f
