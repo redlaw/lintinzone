86002235e54f481d11db3cc0ea68602c7f9676ce
Tam Nguyen <minhtam220@gmail.com>
2012-04-19 15:56:07 +0700

    ok

--- protected/views/layouts/column3.php
+++ protected/views/layouts/column3.php
@@ -53,43 +53,12 @@
     <div class="span-24 ">	
     	<!-- Begin navigation -->	
         <div id="nav" class="span-5">          			
-			<ul>
-			OrderZone
-				<li >
-				<a href="login.html">All Orders</a>
-				</li>
-				<li>
-				<a href="order.html">Your Orders</a>
-				</li>
-				<li>
-				<a href="">Offers</a>
-				</li>
-			</ul>			
+					
         </div>
         <!-- End navigation -->
         
         <!-- Begin bread-content -->
         <div class="span-19 last">
-		
-		<!-- Begin breadcrumb -->	
-		<div id="breadcrumbs" class="span-18">
-		<?php if(isset($this->breadcrumbs)):?>
-			<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-				'links'=>$this->breadcrumbs,
-			)); ?><!-- breadcrumbs -->
-		<?php endif?>
-		This is for breadcrumbs
-		</div>
-		<!-- End  breadcrumb-->
-		<hr style="border: 1px solid #000;" />
-		<!--Begin control_panel-->
-		<div id="control_panel" class="span-18">
-			<input id="button" type="button" value="New Order">
-			<input id="button" type="button" value="New Trip">
-		</div>
-		<!---->
-		<hr style="border: 1px solid #000;" />
-		
 		<!-- Begin content -->
 		<div id="content" class="span-18">
 			<?php echo $content; ?>
